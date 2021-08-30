0472dbd77052747420261d6565cfcab7487c8177
Amy Gori <amydgori@gmail.com>
2021-08-30 18:32:13 -0400

    Add initial models for Recipe, RecipeStep, and Ingredient

--- core/models.py
+++ core/models.py
@@ -1,7 +1,52 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from ordered_model.models import OrderedModel
 
 
 class User(AbstractUser):
     def __str__(self):
         return self.username
+
+
+class Recipe(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="recipes")
+    title = models.CharField(max_length=255)
+    prep_time_in_minutes = models.PositiveIntegerField(null=True, blank=True)
+    cook_time_in_minutes = models.PositiveIntegerField(null=True, blank=True)
+    original_recipe = models.ForeignKey(
+        to="self", on_delete=models.SET_NULL, null=True, blank=True
+    )
+    public = models.BooleanField(default=True)
+    favorited_by = models.ManyToManyField(
+        User, related_name="favorite_recipes", blank=True
+    )
+
+    def total_time_in_minutes(self):
+        if self.cook_time_in_minutes is None or self.prep_time_in_minutes is None:
+            return None
+        return self.cook_time_in_minutes + self.prep_time_in_minutes
+
+    def __str__(self):
+        return self.title
+
+
+class Ingredient(models.Model):
+    recipe = models.ForeignKey(
+        Recipe, on_delete=models.CASCADE, related_name="ingredients"
+    )
+    amount = models.CharField(max_length=20)
+    item = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.amount} {self.item}"
+
+
+class RecipeStep(OrderedModel):
+    recipe = models.ForeignKey(
+        to=Recipe, on_delete=models.CASCADE, related_name="steps"
+    )
+    text = models.TextField()
+    order_with_respect_to = "recipe"
+
+    def __str__(self):
+        return f"{self.order} {self.text}"
